98297f5b130a6cb7ad96ca950f5874a2949a1999
Maksim Okala-Kulak <kaenoru@192.168.100.4>
2019-05-13 02:51:47 +0300

    iPhone layout set

---
+++ View/logoVC.swift
@@ -0,0 +1,17 @@
+//
+//  logoVC.swift
+//  dev-profile
+//
+//  Created by Maksim Okala-Kulak on 5/13/19.
+//  Copyright © 2019 Maksim Okala-Kulak. All rights reserved.
+//
+
+import UIKit
+
+class LogoVC: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 8.0
+    }
+}
